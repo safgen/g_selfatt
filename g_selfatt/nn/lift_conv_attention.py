bb4f907c25fc9a3db5504b98cb689570e8974d35
safgen <sheirafgen@gmail.com>
2024-10-28 05:45:27 +0000

    wip 10.28

--- g_selfatt/nn/lift_conv_attention.py
+++ g_selfatt/nn/lift_conv_attention.py
@@ -205,7 +205,7 @@ class LiftConvAttention(torch.nn.Module):
 
         # Define embeddings.
         if self.conv_embed_layer:
-            self.conv_embed = ConvEmbed(in_chans=self.in_channels, embed_dim=self.mid_channels)
+            self.conv_embed = ConvEmbed(in_chans=self.in_channels, embed_dim=out_channels)
             self.attention = ConvAttention(group=self.group, dim_in=self.mid_channels, dim_out=self.dim_out, num_heads=self.num_heads, attn_drop=attention_dropout_rate)
         else:
             self.conv_embed = ConvEmbed(in_chans=self.in_channels, embed_dim=out_channels, patch_size=3, stride=2)
