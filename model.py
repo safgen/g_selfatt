01c25c8d93461c1a687817929f45be4a92ad14fb
safgen <sheirafgen@gmail.com>
2024-10-28 05:58:43 +0000

    wip

--- model.py
+++ model.py
@@ -127,7 +127,7 @@ def get_model(config):
                 num_channels=12,
                 block_sizes=[0, 1, 2, 1],
                 expansion_per_block=[1, 2, 2, 2],
-                crop_per_layer=[0, 2, 1, 1],
+                crop_per_layer=0, #[0, 2, 1, 1],
                 image_size=image_size,
                 num_classes=num_classes,
                 dropout_rate_after_maxpooling=0.0,
