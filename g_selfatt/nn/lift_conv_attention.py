bd3b74c2061d6b596e1a235a0238b54d745d8237
Sheir <sheirafgen@gmail.com>
2025-02-21 10:22:37 +0900

    pcam related changes

--- g_selfatt/nn/lift_conv_attention.py
+++ g_selfatt/nn/lift_conv_attention.py
@@ -205,7 +205,8 @@ class LiftConvAttention(torch.nn.Module):
 
         # Define embeddings.
         if self.conv_embed_layer:
-            self.conv_embed = ConvEmbed(in_chans=self.in_channels, embed_dim=self.mid_channels, patch_size=patch_size, stride=(patch_size//2) + 1)
+            # self.conv_embed = ConvEmbed(in_chans=self.in_channels, embed_dim=self.mid_channels, patch_size=patch_size, stride=(patch_size//2) + 1)
+            self.conv_embed = GroupEquivariantPatchEmbedding(in_channels=self.in_channels, embed_dim=mid_channels, patch_size=patch_size, stride=(patch_size//2) + 1,rotations=self.group.num_elements )
             self.attention = ConvAttention(group=self.group, dim_in=self.mid_channels, dim_out=self.dim_out, num_heads=self.num_heads, attn_drop=attention_dropout_rate)
         else:
             # self.conv_embed = ConvEmbed(in_chans=self.in_channels, embed_dim=out_channels, patch_size=3, stride=2)
